2449a406ae181f0adf0a5b6583dc7539bf55ad46
Christopher Price <cspprice@gmail.com>
2015-05-08 15:07:56 -0700

    Updated Billboards

---
+++ Assets/Art/Models/Props/Billboard/Images/Thrusters Mat/BThrustersAnimation.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BThrustersAnimation : MonoBehaviour 
+{
+	private float time = 0;
+	private int i = 0;
+
+	public GameObject[] images;
+	public float frame;
+
+
+	void Update ()
+	{
+		time = time + Time.deltaTime;
+
+		if (time > frame) {
+			images [i].SetActive (true);
+			if (i == 3) {
+				images [i - 1].SetActive (false);
+			} else if (i > 0) {
+				images [i - 1].SetActive (false);
+			} else if (i == 0) {
+				images [3].SetActive (false);
+			}
+			time = 0;
+			i++;
+
+			if (i == 4)
+				i = 0;
+		}
+	}
+
+
+}
+
