1e978e122b47ae038f4996a7630a577c425cbedb
Christopher Price <cspprice@gmail.com>
2015-05-08 17:39:04 -0700

    Billboard animation

--- Assets/Art/Models/Props/Billboard/Images/Thrusters Mat/BThrustersAnimation.cs
+++ Assets/Art/Models/Props/Billboard/Images/Thrusters Mat/BThrustersAnimation.cs
@@ -6,21 +6,28 @@ public class BThrustersAnimation : MonoBehaviour
 	private float time = 0;
 	private int i = 0;
 
+
 	public GameObject[] images;
 	public float frame;
-
+	public float seconds;
 
 	void Update ()
 	{
 		time = time + Time.deltaTime;
 
-		if (time > frame) {
+		if (time > frame) 
+		{
 			images [i].SetActive (true);
-			if (i == 3) {
+			if (i == 3) 
+			{
 				images [i - 1].SetActive (false);
-			} else if (i > 0) {
+			} 
+			else if (i > 0) 
+			{
 				images [i - 1].SetActive (false);
-			} else if (i == 0) {
+			} 
+			else if (i == 0) 
+			{
 				images [3].SetActive (false);
 			}
 			time = 0;
@@ -30,7 +37,5 @@ public class BThrustersAnimation : MonoBehaviour
 				i = 0;
 		}
 	}
-
-
 }
 
