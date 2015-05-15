6b2333331c63b2a52d961720534250c7ce95ce50
Jeramy Zapotosky <jzapotos@gmail.com>
2015-05-15 13:42:18 -0700

    Updated icons, audio. and added Raptor cart

---
+++ Assets/RaceAudio.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaceAudio : MonoBehaviour 
+{
+	public AudioSource finalLapSource = null;
+
+	public bool finalLap = false;
+	// Use this for initialization
+	void Awake () 
+	{
+		finalLapSource.volume = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(finalLap)
+		{
+			finalLapSource.volume = 1f;
+		}
+	}
+}
