94ead6570903477a085221ecfcc893dbc53afdfe
Casey Kelly <ckanim3d@gmail.com>
2015-05-21 17:47:56 -0700

    Updated snap to place editor script scenes  ->modified cursa and n600  ->added atlas scene

--- Assets/Editor/RotateToNormalEditor.cs
+++ Assets/Editor/RotateToNormalEditor.cs
@@ -32,6 +32,33 @@ public class RotateToNormalEditor :  MonoBehaviour
 		}
 	}
 
+
+	[MenuItem ("ckTools/Move TM NodesToSnap %u")]
+	public static void MoveTrackmanagerNodesToSnap()
+	{
+		GameObject[] List = Selection.gameObjects;
+		
+		for(int i=0; i<List.Length; i++)
+		{
+			Transform mySelection = List[i].transform;//Selection.activeGameObject.transform;
+			
+			Debug.Log ("Selecting - " + mySelection);
+			
+			Ray ray = new Ray (mySelection.position, mySelection.up * (-1));
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit))
+			{
+				Debug.Log ("Hit - " + hit.normal);
+				//mySelection.rotation = Quaternion.Euler(0,0,0)*hit.normal;
+				mySelection.rotation = Quaternion.FromToRotation (mySelection.up, hit.normal) * mySelection.rotation;
+				mySelection.position = (hit.point + hit.normal*1.2f);
+				//mySelection.Rotate(mySelection.right, 90);// LookRotation(hit.normal)
+				Debug.DrawLine (hit.point, (hit.point + hit.normal), Color.red);
+				
+			}
+		}
+	}
+
 	public static void OnDrawGizmos()
 	{
 
