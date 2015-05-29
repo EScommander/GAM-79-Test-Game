c34f8316c3157b23a6766303e32fdabc7c0aa60b
Casey Kelly <ckanim3d@gmail.com>
2015-05-28 21:48:22 -0700

    Testing dynamic Camera FOV script on Raptor Prefab

---
+++ Assets/Scripts/CasmeraModTunnle.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CasmeraModTunnle : MonoBehaviour {
+
+	private  CartController cartController = null;
+
+	public float normalAngle = 60;
+	public float tunnleAngle = 115;
+
+	private float lerpVal = 0;
+
+	// Use this for initialization
+	void Start ()
+	{
+		cartController = GetComponent<CartController> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		UpdateAnimations ();
+		UpdateCameraFOV ();
+	}
+
+	void UpdateAnimations()
+	{
+		Animator anim = cartController.cartChar.GetComponent<Animator> ();
+		anim.SetFloat ("SpeedInput", lerpVal);
+	}
+
+	void UpdateCameraFOV()
+	{
+		float speed = cartController.Speedometer;
+		float maxSpeed = cartController.topSpeed;
+		
+		lerpVal = speed / maxSpeed;
+		
+		Camera.main.fieldOfView = Mathf.Lerp (normalAngle, tunnleAngle, lerpVal);
+		
+		Debug.Log(lerpVal);
+	}
+}
