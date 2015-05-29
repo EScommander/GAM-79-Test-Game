ff1a1da13b0e251bc991f8628f35c85ee2221ec2
Edison Scott <edison@brokencirclestudios.com>
2015-05-29 11:54:20 -0700

    -BIg Head Test Script

---
+++ Assets/Scripts/BigHeadTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BigHeadTest : MonoBehaviour 
+{
+	private void Awake()
+	{
+		DontDestroyOnLoad (this.gameObject);
+	}
+
+	[ContextMenu("Enable Big Head Mode")]
+	public void EnableBigHeadMode()
+	{
+		ToggleBigHeadMode.BigHeadMode (true);
+	}
+
+	[ContextMenu("Disable Big Head Mode")]
+	public void DisableBigHeadMode()
+	{
+		ToggleBigHeadMode.BigHeadMode (false);
+	}
+}
